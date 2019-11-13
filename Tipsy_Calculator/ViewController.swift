ca5aadf77f91030e0a86bb4f4ca6c96becd891bf
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-13 10:17:42 +0100

    Initial Commit

---
+++ Tipsy_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy_Calculator
+//
+//  Created by Darko Kulakov on 2019-11-13.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
