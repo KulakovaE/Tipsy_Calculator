4c7c6d7b5c79ed67bfe9adf020f67468fef15daa
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-13 12:26:36 +0100

    Working on second UIView

---
+++ Tipsy_Calculator/Controllers/CalculatorViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Tipsy_Calculator
+//
+//  Created by Darko Kulakov on 2019-11-13.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var twelvePctBtn: UIButton!
+    @IBOutlet weak var tenPctBtn: UIButton!
+    @IBOutlet weak var zeroPctBtn: UIButton!
+    @IBOutlet weak var splitNumberlabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func steperValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
+
---
+++ Tipsy_Calculator/Controllers/ResultsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ResultsViewController.swift
+//  Tipsy_Calculator
+//
+//  Created by Darko Kulakov on 2019-11-13.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    
+      @IBOutlet weak var totalLabel: UILabel!
+      @IBOutlet weak var noteHowMuchMoney: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+ 
+     @IBAction func recalculatePressed(_ sender: Any) {
+        
+        
+     }
+}
